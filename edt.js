e6ef8e8d815176ddee477958c3abc653675868c3
Meiian <70202223+meiian@users.noreply.github.com>
2021-02-26 22:13:29 +0100

    swipe added

--- edt.js
+++ edt.js
@@ -2,6 +2,79 @@
              DATA
 ##############################*/
 
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
+
+        this.element.addEventListener('touchstart', function(evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
+    }
+
+    onLeft(callback) {
+        this.onLeft = callback;
+
+        return this;
+    }
+
+    onRight(callback) {
+        this.onRight = callback;
+
+        return this;
+    }
+
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if ( ! this.xDown || ! this.yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
+            if ( this.xDiff > 0 ) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if ( this.yDiff > 0 ) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function(evt) {
+            this.handleTouchMove(evt).bind(this);
+        }.bind(this), false);
+    }
+}
 
 var data = [];  
 function monCode() 
@@ -150,6 +223,20 @@ function main(data) {
         console.log(data);
         changeDate(data);
     });
+    var swiper = new Swipe(document.body);
+    swiper.onLeft(function() {
+         tomorrow = new Date(document.getElementById("inputDate").value);
+         tomorrow.setDate(tomorrow.getDate() + 1);
+         document.getElementById("inputDate").valueAsDate = tomorrow;
+         changeDate(data);
+        });
+    swiper.onRight(function() {
+         yesterday = new Date(document.getElementById("inputDate").value);
+         yesterday.setDate(yesterday.getDate() - 1);
+         document.getElementById("inputDate").valueAsDate = yesterday;
+         changeDate(data);
+    })
+    swiper.run();
 }
 
 
